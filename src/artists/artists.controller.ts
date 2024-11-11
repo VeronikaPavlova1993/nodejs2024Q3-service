9b3d55e6e3d38a3bea87d9d0b016acb737ec06ea
Your Name <veronichka.danilina@mail.ru>
2024-11-11 13:58:02 +0400

    fead: add endpoint for artist

--- src/artists/artists.controller.ts
+++ src/artists/artists.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { ArtistsService } from './artists.service';
+import { CreateArtistDto } from './dto/createArtist.dto';
+import { UpdateArtistDto } from './dto/updateArtist.dto';
 
 @Controller('artist')
-export class ArtistsController {};
+export class ArtistsController {
+  constructor(private readonly artistService: ArtistsService) {}
+
+  @Post()
+  createArtist(@Body() createArtistDto: CreateArtistDto) {
+    return this.artistService.createArtist(createArtistDto);
+  }
+  @Get()
+  getAllArtists() {
+    return this.artistService.getAllArtists();
+  }
+  @Get(':id')
+  getOneArtist(@Param('id') id: string) {
+    return this.artistService.getOneArtist(Number(id));
+  }
+  @Patch(':id')
+  updateArtist(@Param('id') id: string, @Body() updateArtistDto: UpdateArtistDto) {
+    return this.artistService.updateArtist(Number(id), updateArtistDto);
+  }
+  @Delete(':id')
+  removeArtist(@Param('id') id: string) {
+    return this.artistService.removeArtist(Number(id));
+  }
+}
--- src/artists/artists.service.ts
+++ src/artists/artists.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
+import { CreateArtistDto } from './dto/createArtist.dto';
+import { UpdateArtistDto } from './dto/updateArtist.dto';
 
 @Injectable()
-export class ArtistsService {}
+export class ArtistsService {
+  createArtist(createArtistDto: CreateArtistDto) {
+    return 'This action adds a new artist';
+  }
+  getAllArtists() {
+    return `This action returns all artist`;
+  }
+  getOneArtist(id: number) {
+    return `This action returns a #${id} artist`;
+  }
+  updateArtist(id: number, updateArtistDto: UpdateArtistDto) {
+    return `This action updates a #${id} artist`;
+  }
+  removeArtist(id: number) {
+    return `This action removes a #${id} artist`;
+  }
+}
